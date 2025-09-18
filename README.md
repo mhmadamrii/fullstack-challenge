4f279ed6c4f57d5b10364eb6ba5f1679fbe8995d
mhmadamrii <interceptorghost4@gmail.com>
2025-09-18 14:38:00 +0700

    chore(docker): spin the product-service when rabbitmq is ready

---
+++ README.md
@@ -0,0 +1,6 @@
+- rebuild the docker-compose.yml file
+
+```cmd
+docker compose build
+docker compose up -d
+```
--- product-service/src/modules/rabbitmq/rabbitmq.provider.ts
+++ product-service/src/modules/rabbitmq/rabbitmq.provider.ts
@@ -4,15 +4,34 @@ import { Provider } from '@nestjs/common';
 export const RabbitMQProvider: Provider = {
   provide: 'RABBITMQ_CHANNEL',
   useFactory: async () => {
-    const url = process.env.RABBITMQ_URL;
-    const conn = await amqp.connect(url);
-    const channel = await conn.createChannel();
+    const url = process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672';
 
-    await channel.assertExchange('events', 'topic', { durable: true });
-    await channel.assertQueue('order.created', { durable: true });
-    await channel.assertQueue('product.created', { durable: true });
-    await channel.bindQueue('product.created', 'events', 'product.created');
-    await channel.bindQueue('order.created', 'events', 'order.created');
+    let conn: amqp.Connection | null = null;
+    let channel: amqp.Channel | null = null;
+
+    for (let i = 0; i < 5; i++) {
+      // try up to 5 times
+      try {
+        conn = await amqp.connect(url);
+        channel = await conn.createChannel();
+
+        await channel.assertExchange('events', 'topic', { durable: true });
+        await channel.assertQueue('order.created', { durable: true });
+        await channel.assertQueue('product.created', { durable: true });
+        await channel.bindQueue('product.created', 'events', 'product.created');
+        await channel.bindQueue('order.created', 'events', 'order.created');
+
+        console.log('✅ Connected to RabbitMQ');
+        break;
+      } catch (err) {
+        console.error(`❌ RabbitMQ not ready, retrying in 5s... (${i + 1}/5)`);
+        await new Promise((res) => setTimeout(res, 5000));
+      }
+    }
+
+    if (!channel) {
+      throw new Error('Could not connect to RabbitMQ after 5 attempts');
+    }
 
     return channel;
   },
